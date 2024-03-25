8ca32e5f6cce725473a1ccf2cbff8eebe9e2c5b4
Stepan <stepan.sharifulin@mail.ru>
2024-03-25 16:24:05 +0500

    Update
    - Update styles for DataPicker

--- src/04_entities/DatePicker/ui/WhiteDateProvider.tsx
+++ src/04_entities/DatePicker/ui/WhiteDateProvider.tsx
@@ -7,45 +7,9 @@ type WhiteDateProviderProps = {
 
 const theme = createTheme(
     {
-        components: {
-            MuiTextField: {
-                defaultProps: {
-                    variant: 'outlined',
-                    size: 'small'
-                },
-                styleOverrides: {
-                    root: {
-                        '& .MuiOutlinedInput-root': {
-                            '& fieldset': {
-                                borderColor: 'white', // Установка цвета обводки
-                            },
-                            '&:hover fieldset': {
-                                borderColor: 'white', // Цвет обводки при наведении
-                            },
-                            '&.Mui-focused fieldset': {
-                                borderColor: 'white', // Цвет обводки при фокусе
-                            },
-                        },
-                        '& .MuiInputBase-input': {
-                            color: 'white', // Цвет текста, введенного пользователем
-                        },
-                        '& .MuiInputLabel-outlined': {
-                            color: 'grey', // Цвет лейбла до фокуса
-                        },
-                        '& .MuiInputLabel-outlined.Mui-focused': {
-                            color: 'white', // Цвет лейбла при фокусе
-                        }
-                    }
-                }
-            },
-            MuiSvgIcon: {
-                styleOverrides: {
-                    root: {
-                        color: 'white'
-                    }
-                }
-            }
-        }
+        palette: {
+          mode: "dark"
+        },
     }
 )
 
